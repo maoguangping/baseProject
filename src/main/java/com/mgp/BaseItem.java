e6ea4f752a5fe11766ba2c967cbd66f5c797ed52
maoguangping <991335931@qq.com>
2018-05-04 16:45:51 +0800

    first

--- src/main/java/com/mgp/BaseItem.java
+++ src/main/java/com/mgp/BaseItem.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan(basePackages="com.mgp.*.dao")
 public class BaseItem {
-
     public static void main(String[] args) {
         SpringApplication.run(BaseItem.class, args);
     }
-
 }
